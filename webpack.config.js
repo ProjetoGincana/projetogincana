bafdad718eb2bc81585d550c00f9283551952a53
Marcelo <marcelohs180@gmail.com>
2017-02-28 10:02:53 -0300

    Add(config)
    Adding webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,35 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const {join} = require('path')
+const devBuild = process.env.NODE_ENV !== 'production';
+
 module.exports = {
     entry: "./src/client/js/vendors.js",
     output: {
-        path: "./src/java/resources/js/",
-        filename: "bundle.js"
-    }
+        path: join(__dirname, "/src/java/resources/"),
+        filename:  "js/bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000&name=css/fonts/[hash].[ext]' }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
+        new ExtractTextPlugin({
+            filename: 'css/[name]-bundle.css',
+            allChunks: true,
+            disable: devBuild
+        }),
+    ]
 };
